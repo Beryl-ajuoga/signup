ccfe8cd0302668d8bcad82f1a69679abb7cfe439
beryl-ajuoga <berrylajuoga@gmail.com>
2022-05-31 19:35:02 +0300

    addlogin

---
+++ app/src/main/java/dev/beryl/workoutlog/splashActivity.kt
@@ -0,0 +1,13 @@
+package dev.beryl.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent=Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
