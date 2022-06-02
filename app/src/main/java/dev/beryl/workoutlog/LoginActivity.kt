0fdb9e177c03b05fd5c649a98bf41fe987fe3434
beryl-ajuoga <berrylajuoga@gmail.com>
2022-06-02 07:08:30 +0300

    first commit

--- app/src/main/java/dev/beryl/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/beryl/workoutlog/LoginActivity.kt
@@ -1,11 +1,53 @@
 package dev.beryl.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.net.PasswordAuthentication
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var etemail: TextInputEditText
+    lateinit var tvSignup: TextView
+    lateinit var etpassword:TextInputEditText
+    lateinit var btnlogin:Button
+    lateinit var tilemail:TextInputLayout
+    lateinit var tilpassword:TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent = Intent(this,LoginActivity::class.java)
         setContentView(R.layout.activity_login)
+        tvSignup = findViewById(R.id.tvSignup)
+        etemail =findViewById(R.id.etemail)
+        btnlogin = findViewById(R.id.btnlogin)
+        etpassword =findViewById(R.id.etpassword)
+        tilemail = findViewById(R.id.tilemail)
+        tvSignup.setOnClickListener{
+            val intent =Intent(this ,signupActivity::class.java)
+            startActivity(intent)
+
+        }
+        btnlogin.setOnClickListener{
+            validateLogin()
+        }
+
+    }
+    fun validateLogin(){
+        var email = etemail.text.toString()
+        var password = etpassword.text.toString()
+
+        if (email.isBlank()){
+            tilemail.error = "Email is required"
+        }
+        if (password.isBlank()){
+            tilpassword.error = "Password is required"
+        }
+
+
     }
 }
---
+++ app/src/main/java/dev/beryl/workoutlog/signupActivity.kt
@@ -0,0 +1,74 @@
+package dev.beryl.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class signupActivity : AppCompatActivity() {
+    lateinit var etpassword:TextInputEditText
+    lateinit var etfirstname: TextInputEditText
+    lateinit var etlastname: TextInputEditText
+    lateinit var etconfirm:TextInputEditText
+    lateinit var etemail:TextInputEditText
+    lateinit var tilemaill:TextInputLayout
+    lateinit var btnsignup:Button
+    lateinit var tilpassword:TextInputLayout
+    lateinit var tilconfirm: TextInputLayout
+    lateinit var tvlogin:TextView
+    lateinit var tilfirstname:TextInputLayout
+    lateinit var tillastname:TextInputLayout
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        tvlogin =findViewById(R.id.tvlogin)
+        etpassword =findViewById(R.id.etpassword)
+        etfirstname =findViewById(R.id.etfirstname)
+        etlastname = findViewById(R.id.etlastname)
+        etemail=findViewById(R.id.etemail)
+        tilemaill=findViewById(R.id.tilemaill)
+        etconfirm= findViewById(R.id.etconfirm)
+        btnsignup = findViewById(R.id.btnsignup)
+        tilpassword = findViewById(R.id.tilpassword)
+        tilfirstname = findViewById(R.id.tilfirstname)
+        tillastname = findViewById(R.id.tillastname)
+        tilconfirm=findViewById(R.id.tilconfirm)
+        tvlogin=findViewById(R.id.tvlogin)
+
+
+
+        tvlogin.setOnClickListener{
+            val intent = Intent( this , signupActivity::class.java)
+            startActivity(intent)
+        }
+        btnsignup.setOnClickListener{
+            validateSignUp()
+        }
+    }
+    fun validateSignUp(){
+        var Password = etpassword.text.toString()
+        var confirmpassword =etconfirm.text.toString()
+        var firstName = etfirstname.text.toString()
+        var lastName = etlastname.text.toString()
+
+        if (confirmpassword.isBlank()){
+            etconfirm.error = "confirmation is required"
+
+        }
+        if (firstName.isBlank()){
+            etfirstname.error = "First name is required"
+        }
+        if (lastName.isBlank()){
+            etlastname.error = "Last name is required"
+        }
+
+    }
+
+}
